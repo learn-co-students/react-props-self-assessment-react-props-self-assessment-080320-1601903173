716ec65c40e8e0451eec86a372cac1287a87d2fb
AmSmo <adam.smolenski@gmail.com>
2020-10-05 09:55:54 -0400

    works as function and class

--- src/Components/Note.js
+++ src/Components/Note.js
@@ -2,8 +2,18 @@ import React from 'react'
 
 
 
-function note(props) {
-    return <li>{props}</li>
-}
+// function note(props) {
+//     return <li>{props}</li>
+// }
 
-export default note
+// export default note
+
+export default class Note extends React.Component{
+
+    render(){
+    return (
+        <li>
+            {this.props.content}
+            </li>
+            )}
+}
--- src/Components/NotesContainer.js
+++ src/Components/NotesContainer.js
@@ -1,18 +1,22 @@
 import React from 'react'
-import note from "./Note"
-
+// import note from "./Note"
+import Note from "./Note"
 export default class NotesContainer extends React.Component {
 
     apiResponse = () => {
         return ["First Note", "Second Note", "Third Note", "Fourth Note"]
     }
 
+    // allNotes = () => {
+    //     return this.apiResponse().map( function(props){
+    //         return note(props)
+    //     })
+    // }
     allNotes = () => {
-        return this.apiResponse().map( function(props){
-            return note(props)
+        return this.apiResponse().map(function(props){
+            return <Note content={props} />
         })
     }
-
         render() {return(
         <ul>
             {this.allNotes()}
