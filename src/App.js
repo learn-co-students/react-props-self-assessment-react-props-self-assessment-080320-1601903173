c9fce3eb90e81669475b49032b299187493bc064
rachaelghorbani <rachael.ghorbani@gmail.com>
2020-10-05 10:05:43 -0400

    deliverables but still very confused

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from "react";
 import "./App.css";
 import Header from './Components/Header'
+import NotesContainer from './Components/NotesContainer'
 
 class App extends Component {
   render() {
-    return <header />
+      console.log(this.props)
+    return (
+        <div>
+            <Header header="headerProp" />
+            <NotesContainer />
+        </div>
+    )
   }
 }
 
--- src/Components/NotesContainer.js
+++ src/Components/NotesContainer.js
@@ -1,13 +1,22 @@
 import React from 'react'
+import Note from './Note'
 
 class NotesContainer extends React.Component {
 
     apiResponse = () => {
         return ["First Note", "Second Note", "Third Note", "Fourth Note"]
     }
-
+    render(){
+        
     return(
-        <ul></ul >
+        <ul>
+
+            {this.apiResponse().map(resp => {
+                    return < Note content={resp}/>
+                })}
+        </ul >
     )
+    }
 
 }
+export default NotesContainer
